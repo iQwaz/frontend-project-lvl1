8fdd1500ade582eb37efdf62c6d470f2575cb4e7
Kirill Kozedub <dev@kozedub.ru>
2021-12-05 13:56:10 +0300

    feat: implement prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import play from '../src/games/prime-game.js';
+
+play();
---
+++ src/games/prime-game.js
@@ -0,0 +1,23 @@
+import { getRandomInt, play } from '../index.js';
+
+const isPrime = (num) => {
+  let primeCheck = true;
+  let i = 2;
+  while ((i * i <= num) && (primeCheck !== false)) {
+    if (num % i === 0) {
+      primeCheck = false;
+    }
+    i += 1;
+  }
+  return primeCheck;
+};
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const gameOptions = () => {
+  const question = getRandomInt(25);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default () => play([description, gameOptions]);
