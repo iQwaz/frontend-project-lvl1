113106b380040f2c5112ce5e98f8f01f3ba80497
Kirill Kozedub <dev@kozedub.ru>
2021-12-07 00:02:30 +0300

    fix: correct isPrime when num equal 1 or less

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -3,7 +3,7 @@ import { getRandomInt, play } from '../index.js';
 const isPrime = (num) => {
   let primeCheck = true;
   let i = 2;
-  if (num === 1) {
+  if (num <= 1) {
     return false;
   }
   while ((i * i <= num) && (primeCheck !== false)) {
