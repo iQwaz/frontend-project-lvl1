56b3e5f0d3fd122d69394579522e000a1a1077d1
Kirill Kozedub <dev@kozedub.ru>
2021-12-06 23:40:21 +0300

    fix: correct GCD calculation

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -3,6 +3,9 @@ import { getRandomInt, play } from '../index.js';
 const calculateGCD = (num1, num2) => {
   const maxNum = Math.max(num1, num2);
   const minNum = Math.min(num1, num2);
+  if (minNum === 0) {
+    return maxNum;
+  }
   if (maxNum % minNum === 0) {
     return minNum;
   }
