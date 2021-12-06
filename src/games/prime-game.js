c25e280ee057e257a0d9c7e79223fef23dbd94c7
Kirill Kozedub <dev@kozedub.ru>
2021-12-06 23:53:46 +0300

    fix: correct isPrime function

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -3,6 +3,9 @@ import { getRandomInt, play } from '../index.js';
 const isPrime = (num) => {
   let primeCheck = true;
   let i = 2;
+  if (num === 1) {
+    return false;
+  }
   while ((i * i <= num) && (primeCheck !== false)) {
     if (num % i === 0) {
       primeCheck = false;
